682ee4301d085c790b7edd5613687a974a4cc32d
muradhaji <muradhajiyev42@icloud.com>
2024-02-17 18:13:30 +0400

    conditions and bitwise operators explained

---
+++ JavaScript/JS 6/js/conditions.js
@@ -0,0 +1,94 @@
+// let num = Number(prompt("Enter num:"));
+// let message = "Positive";
+
+// if(num < 0) {
+// 	alert("Negative");
+// 	message = "Negative";
+// 	num = Number(prompt("Enter num:"));
+// };
+
+// alert(num);
+
+// if(" ") {
+// 	alert("Negative");
+// }
+
+// if(undefined) {
+// 	alert("Negative");
+// }
+
+// if(num < 0) {
+// 	alert("Negative");
+// } else {
+// 	alert("Positive");
+// };
+
+// if(num < 0) {
+// 	alert("Negative");
+// } else {
+// 	if(num == 0) {
+// 		alert("Neutral");
+// 	} else {
+// 		alert("Positive");
+// 	};
+// };
+
+// if() {} else if() {} else {};
+
+// if(num < 0) {
+// 	alert("Negative");
+// } else if(num > 0) {
+// 	alert("Positive");
+// } else {
+// 	alert("Neutral");
+// }
+
+// let num = prompt("Enter num:");
+
+// if(num == null) {
+// 	alert("You have cancelled.")
+// } else if(isNaN(Number(num))) {
+// 	alert("You have entered wrong number.")
+// } else if(Number(num) > 0) {
+// 	alert("Positive");
+// } else if(Number(num) == 0) {
+// 	alert("Neutral");
+// } else if(Number(num) < 0) {
+// 	alert("Negative");
+// } else {
+// 	alert("Unknown input.");
+// }
+
+// let age = Number(prompt("Enter your age:"));
+// let message = "Not allowed";
+
+// if(age >= 18) {
+// 	message = "Allowed";
+// };
+
+// let message = (age >= 18) ? "Allowed" : "Not Allowed" ;
+// alert(message);
+
+// alert((age >= 18) ? "Allowed" : "Not Allowed");
+
+// (age > = 18) ? alert("Allowed") : alert("Not Allowed");
+
+let num = prompt('Enter num:');
+
+// alert((num < 0) ? "Ngative" : (num > 0) ? "Positive" : "Neutral");
+
+// alert(
+//   num == null
+//     ? 'You have cancelled'
+//     : isNaN(Number(num))
+//     ? 'You have entered wrong number'
+//     : Number(num) > 0
+//     ? 'Positive'
+//     : Number(num) < 0
+//     ? Number(num) < -200
+//       ? 'Less than -200'
+//       : 'Between -1 & -200'
+//     : Number(num) == 0
+//     ? 'Neutral'
+//     : 'Unknown input'
+// );
